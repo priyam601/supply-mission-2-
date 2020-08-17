d49676f17a6c7c528e624a50e3c7cce1cb0e5868
priyam601 <68382598+priyam601@users.noreply.github.com>
2020-08-17 23:07:19 +0530

    Add files via upload

---
+++ SupplyMission-master/sketch.js
@@ -0,0 +1,86 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG,paddle1,paddle2,paddle3;
+var packageBody,ground
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	paddle1 = createSprite(300,630,10,70);
+	paddle1.shapeColor = ("red")
+
+	paddle2 = createSprite(500,630,10,70);
+	paddle2.shapeColor = ("red")
+
+	paddle3 = createSprite(400,660,190,10);
+	paddle3.shapeColor = ("red")
+	
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(400 , 200 , 5 , {restitution:0, isStatic:true});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+	 World.add(world, ground);
+	 
+	 
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+  drawSprites();
+
+  console.log(packageBody);
+
+
+
+  if (keyDown(DOWN_ARROW)) {
+	Matter.Body.setStatic(packageBody,false )
+	
+	  
+	
+	}
+
+
+ 
+}
+
+	
+	
+ 
+
+
+
+
